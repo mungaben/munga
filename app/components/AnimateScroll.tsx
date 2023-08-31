f132550c4feb62c804aa871104e34f4f559c4306
mungaben <kamaumungai3742@gmail.com>
2023-09-01 00:46:56 +0300

    feat :::

--- app/components/AnimateScroll.tsx
+++ app/components/AnimateScroll.tsx
@@ -9,10 +9,12 @@ import { Phrase } from '../lib/Phrase'
 const AnimateScroll = () => {
   return (
     <div className=' bg-gradient-to-r to-violet-500 bg-clip-text text-transparent dark:bg-gradient-to-br dark: from-slate-300   '>
+
         <div className=' text-2xl font-[1000px] max-w-[300px]'>
 
         {Phrase}
         </div>
+        
               
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
 
 
-        <div className=' flex  items-center justify-center h-[60vh] '>
+        <div className=' flex  items-center justify-center h-[60vh]  '>
           <div className=''>
           <AnimateScroll/>
 
